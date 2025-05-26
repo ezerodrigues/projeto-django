c3ea0285ea0ba7b1fa14efc5a481828bd1c1ce9a
ezerodrigues <ezerodrigues@msn.com>
2025-05-26 12:18:17 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse ('<h2>Sobre</h2>')
-
-def contato(request):
-    return HttpResponse ('<h2>Contato</h2>')
+    return render(request, 'recipes/pages/home.html')
